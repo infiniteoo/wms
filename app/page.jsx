cf048d1ebe6e76f6423a442fe084a70b9aea4e4d
infiniteoo <troydorman@gmail.com>
2023-10-16 23:07:41 -0600

    tabs work

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,14 @@
 import { UserButton } from "@clerk/nextjs";
 import { SignUp } from "@clerk/nextjs";
- 
+import { Dashboard } from "../components/Dashboard";
+import Image from "next/image";
+import logo from '../public/gb2.png'
+
 export default function Home() {
   return (
-    <div >
-     
-      <UserButton afterSignOutUrl="/"/>
+    <div className="flex flex-row justify-between p-2 w-100 h-100">
+      <Dashboard />
+      <UserButton afterSignOutUrl="/" />
     </div>
-  )
+  );
 }
