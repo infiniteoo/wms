3a6dde4b4f117e5a472d2d2af17de048e2c8a801
infiniteoo <troydorman@gmail.com>
2023-10-23 17:38:41 -0600

    added recent activity tab to inventory dashboard

--- components/inventory/InventoryDashboard.jsx
+++ components/inventory/InventoryDashboard.jsx
@@ -1,14 +1,22 @@
-
 import OccupancyProgress from "./OccupancyProgress";
+import RecentActivity from "./RecentActivity";
 
 const InventoryDashboard = () => {
-  
-return (
-    <div className="p-4 w-full">
-      <OccupancyProgress />
+  return (
+    <div className="p-4  flex flex-col w-screen">
+      <div className="p-4 flex flex-row">
+        <div className="w-1/2">
+          {" "}
+          <OccupancyProgress />
+        </div>
+
+        <div className="w-1/2">
+          {" "}
+          <RecentActivity />
+        </div>
+      </div>
     </div>
-)
-  
+  );
 };
 
 export default InventoryDashboard;
---
+++ components/inventory/RecentActivity.jsx
@@ -0,0 +1,48 @@
+import { supabase } from "../../supabase";
+import { useState, useEffect } from "react";
+
+const RecentActivity = () => {
+  const [activity, setActivity] = useState([]);
+
+  useEffect(() => {
+    async function fetchRecentActivity() {
+      try {
+        const { data, error } = await supabase
+          .from("inventory")
+          .select("*")
+          .order("last_modified", { ascending: true })
+          .limit(5);
+
+        if (error) {
+          console.error(error);
+        } else {
+          setActivity(data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchRecentActivity();
+  }, []);
+
+  return (
+    <div className="bg-white rounded-lg p-4 shadow-md">
+    <div className="recent-activity">
+    <h2 className="text-lg font-semibold">Recent Activity</h2>
+      <div className="console">
+        {activity.map((item) => (
+          <div key={item.id} className="console-line">
+            <span className="console-timestamp">
+              {new Date(item.last_modified).toLocaleString()}
+            </span>
+            <span className="console-message">{`Updated Item: ${item.lpn_number} - By: ${item.lastTouchedBy}`}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default RecentActivity;
