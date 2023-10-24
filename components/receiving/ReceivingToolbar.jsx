70625596b72725625d619e3c5d1197155a60fcbf
infiniteoo <troydorman@gmail.com>
2023-10-24 12:07:40 -0600

    hide completed switch

--- components/receiving/ReceivingToolbar.jsx
+++ components/receiving/ReceivingToolbar.jsx
@@ -19,6 +19,16 @@ const ReceivingToolbar = ({
   setShowDeleteModal,
   fetchInventory,
 }) => {
+  const [hideCompleted, setHideCompleted] = useState(true);
+
+  const toggleHideCompleted = () => {
+    setHideCompleted(!hideCompleted);
+  };
+
+  const filteredInventory = hideCompleted
+    ? inventory.filter((item) => !item.completed)
+    : inventory;
+
   return (
     <div className="flex flex-row justify-between p-1 border-cyan-400">
       <div className="flex items-center">
@@ -30,18 +40,37 @@ const ReceivingToolbar = ({
           setSelectedRows={setSelectedRows}
           setItemToDelete={setItemToDelete}
           setShowDeleteModal={setShowDeleteModal}
-
         />
       </div>
 
       <div className="flex items-center">
-        <RefreshButton setInventory={setInventory} fetchInventory={fetchInventory} />
+        <div className={`switch-container mr-10 `}>
+          <label
+            className={`switch ${
+              hideCompleted
+                ? "border-2 border-green-400"
+                : "border-2 border-red-400"
+            }`}
+          >
+            <input
+              type="checkbox"
+              checked={hideCompleted}
+              onChange={toggleHideCompleted}
+              title="Hide Completed"
+            />
+            <span className={`slider`}></span>
+          </label>
+        </div>
+        <RefreshButton
+          setInventory={setInventory}
+          fetchInventory={fetchInventory}
+        />
         <SearchModifierDropdown
-          inventory={inventory}
+          inventory={filteredInventory}
           setModifier={setModifier}
         />
         <SearchTextInput
-          inventory={inventory}
+          inventory={filteredInventory}
           setInventory={setInventory}
           setSearchTerm={setSearchTerm}
           searchTerm={searchTerm}
