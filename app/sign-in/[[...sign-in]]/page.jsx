8ab71e976677cf88b6f21a05c64f1c97c72afbad
infiniteoo <troydorman@gmail.com>
2023-11-12 14:44:09 -0700

    added image to sign up / in pages

--- app/sign-in/[[...sign-in]]/page.jsx
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
 import { SignIn } from "@clerk/nextjs";
-
 
 export default function Page() {
   return (
-    
-      <div className="flex justify-center items-center ml-2">
-        <SignIn />
+    <div className="flex">
+      {/* Background Image */}
+      <div
+        className="flex-grow bg-left  "
+        style={{
+          backgroundImage: `url('/gb-splash.png')`,
+          width: "100%", // Adjust the width as needed
+          height: "100vh", // Adjust the height as needed
+        }}
+      ></div>
+
+      {/* SignIn Component */}
+      <div className="flex-grow">
+        <div className="flex justify-center items-center h-screen">
+          <SignIn />
+        </div>
       </div>
-    
+    </div>
   );
 }
--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
 import { SignUp } from "@clerk/nextjs";
-
 
 export default function Page() {
   return (
-    
-      <div className="flex justify-center items-center ml-2">
-        <SignUp />
+    <div className="flex">
+      {/* Background Image */}
+      <div
+        className="flex-grow bg-left  "
+        style={{
+          backgroundImage: `url('/gb-splash.png')`,
+          width: "100%", // Adjust the width as needed
+          height: "100vh", // Adjust the height as needed
+        }}
+      ></div>
+
+      {/* SignUp Component */}
+      <div className="flex-grow">
+        <div className="flex justify-center items-center h-screen">
+          <SignUp />
+        </div>
       </div>
-    
+    </div>
   );
 }
