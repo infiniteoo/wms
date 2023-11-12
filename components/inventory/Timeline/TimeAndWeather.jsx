b12a31e07d9ba1b6eb711af103d8eca98b10f588
infiniteoo <troydorman@gmail.com>
2023-11-11 23:58:27 -0700

    time component

--- components/inventory/Timeline/TimeAndWeather.jsx
+++ components/inventory/Timeline/TimeAndWeather.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import TimeComponent from "./TimeComponent.jsx";
+/* import TimeComponent from "./TimeComponent.jsx"; */
 import WeatherComponent from "./WeatherComponent";
 
 const TimeAndWeather = () => {
   return (
     <div className=" flex flex-col items-left justify-left">
-      <div className="text-lg p-1 text-left">
-        <TimeComponent />
-      </div>
+      <div className="text-lg p-1 text-left">{/*  <TimeComponent /> */}</div>
       <div className="mt-3">
         <WeatherComponent
           apiKey={process.env.NEXT_PUBLIC_WEATHER_API_KEY}
