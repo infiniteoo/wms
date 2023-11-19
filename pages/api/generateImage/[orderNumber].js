ff1a66ea495ba179c384bdced5b257e025f8ae64
infiniteoo <troydorman@gmail.com>
2023-11-18 19:34:04 -0700

    barcode generation route works now just having trouble generating the images

--- pages/api/generateImage/[orderNumber].js
+++ pages/api/generateImage/[orderNumber].js
@@ -6,19 +6,19 @@ export default async function handler(req, res) {
   console.log("hello from generateImage handler");
   console.log("req", req);
 
-  /* try { */
-  let completed = barcodeGenerator(orderNumber);
-  console.log("completed before", completed);
-  if (completed) {
-    console.log("completed after", completed);
-    res.status(200).json({ success: true });
-  } else {
-    res
-      .status(400)
-      .json({ success: false, error: "Barcode generation failed" });
-  }
-  /*   } catch (error) {
+  try {
+    let completed = barcodeGenerator(orderNumber);
+    console.log("completed before", completed);
+    if (completed) {
+      console.log("completed after", completed);
+      res.status(200).json({ success: true });
+    } else {
+      res
+        .status(400)
+        .json({ success: false, error: "Barcode generation failed" });
+    }
+  } catch (error) {
     console.error("Error during barcode generation:", error);
     res.status(500).json({ success: false, error: "Internal server error" });
-  } */
+  }
 }
